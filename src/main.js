e35601254f416d4805387a255d57816eb8a6a0bb
Kevin Caria <kcaria@pigmalion.com.ar>
2025-06-29 18:32:46 -0300

    se agrega soporte para CORS y se actualizan las dependencias en package.json y package-lock.json; se agrega .gitignore

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const cors = require("cors")
 const db = require("./db/models")
 const app = express()
 const swaggerUi = require("swagger-ui-express")
@@ -15,6 +16,7 @@ const {
 } = require("./routes")
 
 const PORT = process.env.PORT || 3001
+app.use(cors({ origin: 'http://localhost:5173' }))
 app.use(express.json())
 app.use("/users", userRoute)
 app.use("/post", postRoute)
