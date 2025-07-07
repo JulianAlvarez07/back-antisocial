f880fab147ee514c6a111a443e0fe2cd9669c764
JulianAlvarez07 <alvarezjuli30@gmail.com>
2025-07-07 14:33:31 -0300

    Update src/main.js

--- src/main.js
+++ src/main.js
@@ -16,9 +16,14 @@ const {
 } = require("./routes");
 
 const PORT = process.env.PORT || 3001;
-app.use(cors({ origin: "http://localhost:5173" }));
+
+app.use(cors()); // Solo esta línea para que no haya problema CORS
 app.use(express.json());
-app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API corriendo 🎉");
+});
+
 app.use("/users", userRoute);
 app.use("/post", postRoute);
 app.use("/post-images", postImagesRoute);
@@ -29,5 +34,5 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server corriendo en el puerto: ${PORT}`);
-  //db.sequelize.sync({ force: true })
+  // db.sequelize.sync({ force: true });
 });
