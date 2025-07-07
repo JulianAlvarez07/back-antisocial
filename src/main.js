d2af38ba44b5b3bf8f63944d5237c4904c3bc3b1
JulianAlvarez07 <alvarezjuli30@gmail.com>
2025-07-07 14:29:28 -0300

    Update src/main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-const express = require("express")
-const cors = require("cors")
-const db = require("./db/models")
-const app = express()
-const swaggerUi = require("swagger-ui-express")
-const YAML = require("yamljs")
-const path = require("path")
-const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"))
+const express = require("express");
+const cors = require("cors");
+const db = require("./db/models");
+const app = express();
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const path = require("path");
+const swaggerDocument = YAML.load(path.join(__dirname, "../swagger.yaml"));
 const {
   userRoute,
   postRoute,
@@ -13,20 +13,21 @@ const {
   commentRoute,
   tagRoute,
   commentTagRoute,
-} = require("./routes")
+} = require("./routes");
 
-const PORT = process.env.PORT || 3001
-app.use(cors({ origin: 'http://localhost:5173' }))
-app.use(express.json())
-app.use(cors())
-app.use("/users", userRoute)
-app.use("/post", postRoute)
-app.use("/post-images", postImagesRoute)
-app.use("/comments", commentRoute)
-app.use("/tags", tagRoute)
-app.use("/comment-tags", commentTagRoute)
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-app.listen(PORT, () => {
-  console.log(`Server corriendo en el puerto: ${PORT}`)
+const PORT = process.env.PORT || 3001;
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+app.use(cors());
+app.use("/users", userRoute);
+app.use("/post", postRoute);
+app.use("/post-images", postImagesRoute);
+app.use("/comments", commentRoute);
+app.use("/tags", tagRoute);
+app.use("/comment-tags", commentTagRoute);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server corriendo en el puerto: ${PORT}`);
   //db.sequelize.sync({ force: true })
-})
+});
